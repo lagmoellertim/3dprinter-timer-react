58a737ef063fc57f124b994132c747b242cedf5a
Tim-Luca Lagmöller <lagmoellertim@gmail.com>
2019-04-04 21:16:27 +0200

    Release added

---
+++ precache-manifest.2e4f7ba8dca439d0252c1ac6a85eb0d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e03525b5338cb8e6ff4",
+    "url": "/static/css/main.931197c0.chunk.css"
+  },
+  {
+    "revision": "8e03525b5338cb8e6ff4",
+    "url": "/static/js/main.a1381e54.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0975e32ca60370c0b14d",
+    "url": "/static/css/2.781b4c6c.chunk.css"
+  },
+  {
+    "revision": "0975e32ca60370c0b14d",
+    "url": "/static/js/2.01255942.chunk.js"
+  },
+  {
+    "revision": "de9b4999c63b1cb7da2a0eba70158010",
+    "url": "/index.html"
+  }
+];
